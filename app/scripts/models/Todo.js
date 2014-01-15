c81149b15c3fc3ec5e11b69620143fc2b903e69f
greg stasica <gstasica@gmail.com>
2014-01-15 21:48:47 +0000

    first commit

---
+++ app/scripts/models/Todo.js
@@ -0,0 +1,12 @@
+/*global todoBackbonePoc1, Backbone*/
+
+todoBackbonePoc1.Models = todoBackbonePoc1.Models || {};
+
+(function () {
+    'use strict';
+
+    todoBackbonePoc1.Models.TodoModel = Backbone.Model.extend({
+
+    });
+
+})();
